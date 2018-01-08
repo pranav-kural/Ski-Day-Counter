ed8d353c185b30c7e0dcf580a9d2708a9545acbc
Pranav Kural <pranavkural042@gmail.com>
2018-01-08 10:22:34 -0500

    Added properties to the SkiDayComponent

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { SkiDayComponent } from "./components/SkiDayCount";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <SkiDayComponent total={50}
+                            powder={20}
+                            backcountry={10}
+                            goal={100} />
       </div>
     );
   }
--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,4 +1,29 @@
-import { React } from "react";
-import "../stylesheets/ui.scss";
+import React, { Component } from 'react'
+import "../stylesheets/ui.css";
 
 // SkiDayComponent
+export class SkiDayComponent extends Component {
+
+    percentToDecimal = (decimal) => ((decimal * 100) + "%")
+
+    calcGoalProgress = (total, goal) => this.percentToDecimal(total/goal)
+
+    render() {
+        return (
+            <div className="ski-day-count">
+                <div className="total-days">
+                    <span>{this.props.total} days</span>
+                </div>
+                <div className="powder-days">
+                    <span>{this.props.powder} days</span>                    
+                </div>
+                <div className="backcountry-days">
+                    <span>{this.props.backcountry} days</span>                                    
+                </div>
+                <div>
+                    <span>{this.calcGoalProgress(this.props.total, this.props.goal)}</span>
+                </div>
+            </div>
+        )
+    }
+}
