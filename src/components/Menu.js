67ad35c902594464b857e67dd01c502700d4a5cb
Pranav Kural <pranavkural042@gmail.com>
2018-04-14 17:01:32 -0400

    replaced Link to NavLink to enable active styling

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import HomeIcon from 'react-icons/lib/fa/home'
 import AddDayIcon from 'react-icons/lib/fa/calendar-plus-o'
 import ListDaysIcon from 'react-icons/lib/fa/table'
 
 export const Menu = () => 
     <nav className="menu">
-        <Link to='/' activeClassName='selected'><HomeIcon /></Link>
-        <Link to='/add-day' activeClassName='selected'><AddDayIcon /></Link>
-        <Link to='/list-days' activeClassName='selected'><ListDaysIcon /></Link>
+        <NavLink to='/' activeClassName='selected'><HomeIcon /></NavLink>
+        <NavLink to='/add-day' activeClassName='selected'><AddDayIcon /></NavLink>
+        <NavLink to='/list-days' activeClassName='selected'><ListDaysIcon /></NavLink>
 
     </nav>
